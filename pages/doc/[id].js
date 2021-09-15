3beaae2c88d3d0fb088e47626b8f6f2673101f52
danildany <76399818+danildany@users.noreply.github.com>
2021-09-15 00:05:23 -0300

    add text editor

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -5,6 +5,7 @@ import { db } from "../../firebase";
 import { useDocumentOnce } from "react-firebase-hooks/firestore";
 import { getSession, useSession, signOut } from "next-auth/client";
 import Login from "../../components/Login";
+import TextEditor from "../../components/TextEditor";
 
 function Doc() {
   const [session] = useSession();
@@ -15,7 +16,9 @@ function Doc() {
   const [snapshot, loadingSnapshot] = useDocumentOnce(
     db.collection("userDocs").doc(session.user.email).collection("docs").doc(id)
   );
-
+  if (!loadingSnapshot && !snapshot?.data()?.fileName) {
+    router.replace("/");
+  }
   return (
     <div>
       <header className="flex justify-between items-center p-3 pb-1">
@@ -24,10 +27,46 @@ function Doc() {
         </span>
         <div className="flex-grow px-2">
           <h2>{snapshot?.data()?.fileName}</h2>
+          <div className="flex items-center text-sm space-x-1 -ml-1 h-8 text-gray-700">
+            <p className="option">File</p>
+            <p className="option">Edit</p>
+            <p className="option">View</p>
+            <p className="option">Insert</p>
+            <p className="option">Format</p>
+            <p className="option">Tool</p>
+          </div>
         </div>
+        <Button
+          color="lightBlue"
+          buttonType="filled"
+          size="regular"
+          iconOnly={false}
+          rounded={false}
+          block={false}
+          ripple="light"
+          className="hidden md:inline-flex h-10"
+        >
+          <Icon name="people" size="md" />
+          SHARE
+        </Button>
+        <img
+          className="rounded-full cursor-pointer h-10 w-10 ml-10"
+          src={session.user.image}
+          alt="profile picture"
+        />
       </header>
+      <TextEditor />
     </div>
   );
 }
 
 export default Doc;
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
